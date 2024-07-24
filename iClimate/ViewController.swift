da5537341bce3373f6e708220d778b59afad2e3c
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-07-24 14:54:18 +0500

    Initial Commit

---
+++ iClimate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iClimate
+//
+//  Created by Bakhrom Usmanov on 24/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
